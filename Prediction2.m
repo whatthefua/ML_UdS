891bc68c6702af0d8d3c9c1fd43f14aa2bcd59df
whatthefua <arnan_s@msn.com>
2018-11-09 13:37:34 +0100

    Correction

--- Prediction2.m
+++ Prediction2.m
@@ -3,10 +3,10 @@
 function y = Prediction2(x, W1, W2)
 	nSamples = size(x, 2);
 
-	xB = horzcat(x, ones(1, nSamples));
+	xB = horzcat(x, ones(nSamples, 1));
 
 	hidden1 = ReLU(xB * W1);
-	hidden1B = horzcat(hidden1, ones(1, nSamples));
+	hidden1B = horzcat(hidden1, ones(nSamples, 1));
 
 	y = ReLU(hidden1B * W2);
 end
--- Train2.m
+++ Train2.m
@@ -1,5 +1,7 @@
 % Train function of 3-Layer FNN
 
 function [W1, W2] = Train2(x, y)
-	
+	nSamples = size(x, 2);
+
+	xB = horzcat(x, ones(nSamples, 1));
 end
